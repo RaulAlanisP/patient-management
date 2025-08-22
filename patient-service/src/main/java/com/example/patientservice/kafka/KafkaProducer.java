1c923604f1414f504378e3a1f70b9aea33fc80ea
RaulAlanisP <raul.alanis.gin@gmail.com>
2025-08-22 13:21:46 -0600

    33.define-kafka-patient-event-proto-file

---
+++ patient-service/src/main/java/com/example/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,18 @@
+package com.example.patientservice.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(
+            KafkaProducer.class);
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+}
